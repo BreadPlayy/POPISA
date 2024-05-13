030ac9dd879810c859a1e167247271db55816ce5
BreadPlayy <61905841+BreadPlayy@users.noreply.github.com>
2024-05-13 20:10:10 +0300

    Add files via upload

---
+++ sabibor.lua
@@ -0,0 +1,135 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Poison Hub", "Ocean")
+    -- MAIN
+    local Main = Window:NewTab("Auto Farm")
+    local MainSection = Main:NewSection("Farm Settings")
+
+
+    MainSection:NewToggle("Auto Rank", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Buy Area", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Teleport Area", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Tap", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewSlider("Tap Area", "SPEED!!", 200, 16, function()
+    end)
+
+    MainSection:NewToggle("Auto Rebirth", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Fruit", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+    MainSection:NewToggle("Auto Flag", "Makes you do gymnastics", function()
+    end)
+	
+
+    -- Huge Dupe
+    local DupeH = Window:NewTab("Huge Dupe")
+    local DupeHSection = DupeH:NewSection("Huge Dupe (Only Huges!)")	
+
+    DupeHSection:NewButton("Dupe Huge", "Makes you do gymnastics", function()
+		loadstring(game:HttpGet(('https://pastebin.com/raw/zg9S6j2n'),true))()
+    end)
+
+	DupeHSection:NewTextBox("Chose Huge", "Enter Name", function()
+	end)
+
+
+    -- Titanic Dupe
+    local DupeT = Window:NewTab("Titanic Dupe")
+    local DupeTSection = DupeT:NewSection("Titanic Dupe (Titanics Only!)")
+
+    DupeTSection:NewButton("Dupe Titanic", "Makes you do gymnastics", function()
+    end)
+
+	DupeTSection:NewTextBox("Chose Titanic", "Enter Name", function()
+	end)
+
+
+    -- Diamond Dupe
+    local DupeD = Window:NewTab("Diamond Dupe")
+    local DupeDSection = DupeD:NewSection("Diamond Dupe")	
+
+    DupeDSection:NewButton("Dupe Diamonds", "Makes you do gymnastics", function()
+    end)
+
+    DupeDSection:NewToggle("Dupe x2", "Makes you do gymnastics", function()
+    end)
+
+    DupeDSection:NewToggle("Dupe x5", "Makes you do gymnastics", function()
+    end)
+
+    DupeDSection:NewToggle("Dupe x10", "Makes you do gymnastics", function()
+    end)
+
+
+    -- Trade Scam
+    local Trade = Window:NewTab("Trade Scam")
+    local TradeSection = Trade:NewSection("After putting pets on Trade Freeze and THEN Trade Swaper")
+
+    TradeSection:NewButton("Trade Freeze", "Makes you do gymnastics", function()
+    end)
+
+    TradeSection:NewButton("Trade Swaper", "Makes you do gymnastics", function()
+    end)
+
+
+    -- Backrooms
+    local Backrooms = Window:NewTab("Backrooms")
+    local BackroomsSection = Backrooms:NewSection("Backrooms")
+
+    BackroomsSection:NewButton("Dupe Backrooms Key", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewButton("Dupe Deep Backrooms Key", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewButton("Find Boss Room", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewToggle("Auto Farm Boss + Chests", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewButton("Find 100x Egg", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewToggle("AutoOpen Nearest Egg", "Makes you do gymnastics", function()
+    end)
+
+    BackroomsSection:NewSlider("Amount Of Eggs", "SPEED!!", 102, 16, function()
+    end)
+
+    -- Credits
+    local Credits = Window:NewTab("Credits")
+    local CreditsSection = Credits:NewSection("Credit by Porion!")
+
+    CreditsSection:NewButton("Join Our Discord", "Makes you do gymnastics", function()
+    end)
